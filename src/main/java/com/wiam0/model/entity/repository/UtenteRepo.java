26f3cfd67463405f2a976180766393efc8fc28e3
PICCIRILLI <simone.a.piccirilli@capgemini.com>
2022-12-05 15:42:53 +0100

    more junit added

--- src/main/java/com/wiam0/model/entity/repository/UtenteRepo.java
+++ src/main/java/com/wiam0/model/entity/repository/UtenteRepo.java
@@ -1,5 +1,7 @@
 package com.wiam0.model.entity.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,16 +13,29 @@ import com.wiam0.model.entity.Utente;
 public interface UtenteRepo extends JpaRepository<Utente, String>{
 
 	@Query(value = "SELECT * FROM utente WHERE utente.username = :username",
-			 nativeQuery = true)
+			nativeQuery = true)
 	Utente findByUsername(@Param("username") String username);
+	
+	@Query(value = "SELECT * FROM utente",
+			nativeQuery = true)
+	List<Utente> findAllUtente();
 	
 	Utente findByBt(String bt);
 	boolean existsByUsername(String username);
-	
+
 	boolean existsByBt(String bt);
 	
+	
 	// queery test Hibernate
-	 @Query(value = "SELECT * FROM wiam0.utente WHERE wiam0.utente.username = :username",
-			 nativeQuery = true)
-		    Utente findByUsernameTest(@Param("username") String username);
+	@Query(value = "SELECT * FROM wiam0.utente WHERE wiam0.utente.username = :username",
+			nativeQuery = true)
+	Utente findByUsernameTest(@Param("username") String username);
+
+	@Query(value = "SELECT * FROM wiam0.utente",
+			nativeQuery = true)
+	List<Utente> findAllTest();
+	
+	@Query(value = "insert into wiam0.utente (id, bt, abi, username, email, cellulare ) values (:id ,:bt, :abi, :username, :email, :cellulare)",
+			nativeQuery = true)
+	void saveUtente(@Param("utente") Utente utente);
 }
--- src/test/java/com/wiam0/db/test/DispoDbTest.java
+++ src/test/java/com/wiam0/db/test/DispoDbTest.java
@@ -2,12 +2,14 @@ package com.wiam0.db.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
@@ -24,12 +26,42 @@ public class DispoDbTest {
 	@Autowired
 	UtenteRepo utenteRepo;
 	
-	// funziona perfettamente, il problema e fargli leggere lo schema, al momento sono tolti ma va trovata fix
+	// funziona perfettamente
 	@Test
 	public void testUtente1() {
 		 Utente response = utenteRepo.findByUsernameTest("usernameProva1");
 		 
 		 System.out.println(response);
 		 assertThat(response.getAbi()).isEqualTo("abiProva1");
+	}
+	
+	@Test
+	public void testUtente2() {
+		 List<Utente> response = utenteRepo.findAllTest();
+		 
+		 System.out.println(response);
+		 
+		Optional<Utente>  iUtente = response.stream().filter(resp -> resp.getUsername().equals("usernameProva1")).findAny();
+		
+		assertThat(iUtente.get().getAbi()).isEqualTo("abiProva1");
+	}
+	
+	//@Test
+	public void testUtente3() {
+		
+		 Utente utente = new Utente();
+		 utente.setAbi("abiTest1");
+		 utente.setBt("btTest1");
+		 utente.setCellulare("cellulareTest1");
+		 utente.setEmail("emailTest1");
+		 utente.setUsername("usernameTest1");
+		 
+		 utenteRepo.saveUtente(utente);
+		 
+		 List<Utente> response = utenteRepo.findAllTest();
+		 
+		Optional<Utente>  iUtente = response.stream().filter(resp -> resp.getUsername().equals("usernameTest1")).findAny();
+		
+		assertThat(iUtente.get().getAbi()).isEqualTo("abiTest1");
 	}
 }
